80bde75b090b9f411832a8c5187e3b84b769fe9c
Raphael Mouton <raphaelmouton@live.fr>
2018-07-19 19:16:46 +0200

    add fav /ok

--- src/Controller/FavoriteController.php
+++ src/Controller/FavoriteController.php
@@ -2,9 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Announce;
 use App\Entity\Favorite;
-use App\Form\Favorite1Type;
-use App\Form\FavoriteType;
 use App\Repository\FavoriteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,75 +15,38 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class FavoriteController extends Controller
 {
-    /**
-     * @Route("/", name="favorite_index", methods="GET")
-     */
-    public function index(FavoriteRepository $favoriteRepository): Response
-    {
-        return $this->render('favorite/index.html.twig', ['favorites' => $favoriteRepository->findAll()]);
-    }
 
     /**
      * @Route("/new/{id}", name="favorite_new", methods="GET|POST")
      */
-    public function new(Request $request, $id): Response
+    public function new(Request $request, Announce $announce): Response
     {
-
-
-        $em = $this->getDoctrine()->getManager();
-        $favorite = $em->getRepository(Favorite::class)->findAll();
-
         $favorite = new Favorite();
+        $em = $this->getDoctrine()->getManager();
         $favorite->setUser($this->getUser());
-        $favorite->setAnnounce($id);
-        $favorite->setActive(true);
+        $favorite->setAnnounce($announce);
+        $favorite->setActive(1);
 
         $em->persist($favorite);
         $em->flush();
 
-        return $this->redirectToRoute('favorite_index');
+        return $this->redirectToRoute('announce_show', ['id' => $announce->getId()]);
 
     }
 
     /**
-     * @Route("/{id}", name="favorite_show", methods="GET")
+     * @Route("/delete/{id}", name="favorite_delete", methods="GET|POST")
      */
-    public function show(Favorite $favorite): Response
+    public function deleteFav(Announce $announce): Response
     {
-        return $this->render('favorite/show.html.twig', ['favorite' => $favorite]);
-    }
+        $favorite = new Favorite();
+        $em = $this->getDoctrine()->getManager();
+        $favorite->setUser($this->getUser());
+        $favorite->setAnnounce($announce);
+        $favorite->setActive(0);
+        $em->persist($favorite);
+        $em->flush();
 
-    /**
-     * @Route("/{id}/edit", name="favorite_edit", methods="GET|POST")
-     */
-    public function edit(Request $request, Favorite $favorite): Response
-    {
-        $form = $this->createForm(FavoriteType::class, $favorite);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('favorite_edit', ['id' => $favorite->getId()]);
-        }
-
-        return $this->render('favorite/edit.html.twig', [
-            'favorite' => $favorite,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="favorite_delete", methods="DELETE")
-     */
-    public function delete(Request $request, Favorite $favorite): Response
-    {
-        if ($this->isCsrfTokenValid('delete' . $favorite->getId(), $request->request->get('_token'))) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($favorite);
-            $em->flush();
-        }
-
-        return $this->redirectToRoute('favorite_index');
+        return $this->redirectToRoute('announce_show', ['id' => $announce->getId()]);
     }
 }
