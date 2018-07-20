324e3525bb83c4634fb1d1fcde17cb298855a487
Raphael Mouton <raphaelmouton@live.fr>
2018-07-20 14:49:48 +0200

    error upload /ok

--- src/Entity/Announce.php
+++ src/Entity/Announce.php
@@ -2,13 +2,12 @@
 
 namespace App\Entity;
 
+use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\HttpFoundation\File\File;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
-use Symfony\Component\Validator\Constraints as Assert;
-
 /**
  * @ORM\Entity(repositoryClass="App\Repository\AnnounceRepository")
  * @Vich\Uploadable
